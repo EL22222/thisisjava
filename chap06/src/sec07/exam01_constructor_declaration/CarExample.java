a90db5589288b2e32acc35d4183be4672c32fe28
vnjl <lsheayenugk1@naver.com>
2022-07-07 11:06:18 +0900

    1106 0707

---
+++ chap06/src/sec07/exam01_constructor_declaration/CarExample.java
@@ -0,0 +1,12 @@
+package sec07.exam01_constructor_declaration;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car myCar = new Car("검정", 3000);
+		
+		//Car myCar = new Car(); (x) 생성자가 없으면 생성자를 알아서 만들어서 오류남
+
+	}
+
+}
---
+++ chap06/src/sec07/exam02_field_intialize/Korean.java
@@ -0,0 +1,23 @@
+package sec07.exam02_field_intialize;
+
+public class Korean {
+	//필드
+	String nation = "대한민국";
+	String name;
+	String ssn;
+	
+	//생성자
+	/*public Korean(String n, String s) {
+	  name = n;
+	  ssn = s;
+	  }
+	 */
+	
+	public Korean(String name, String ssn) {
+		this.name = name;
+		this.ssn = ssn;
+		//this 그때그때 다름 
+		
+	}
+
+}
